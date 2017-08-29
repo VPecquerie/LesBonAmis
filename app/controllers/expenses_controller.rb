88017b8592ed0fb3f7e7f8367a9fbe5fd30c5a14
Florian Guillet <florian.guillet7@gmail.com>
2017-08-29 11:56:51 +0200

    Expense: Remove user input to hidden input and add only connected access

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -4,26 +4,44 @@ class ExpensesController < ApplicationController
   # GET /expenses
   # GET /expenses.json
   def index
+    if !@current_user
+      redirect_to login_path
+    end
+
     @expenses = Expense.all
   end
 
   # GET /expenses/1
   # GET /expenses/1.json
   def show
+    if !@current_user
+      redirect_to login_path
+    end
   end
 
   # GET /expenses/new
   def new
+    if !@current_user
+      redirect_to login_path
+    end
+
     @expense = Expense.new
   end
 
   # GET /expenses/1/edit
   def edit
+    if !@current_user
+      redirect_to login_path
+    end
   end
 
   # POST /expenses
   # POST /expenses.json
   def create
+    if !@current_user
+      redirect_to login_path
+    end
+
     @expense = Expense.new(expense_params)
 
     respond_to do |format|
@@ -40,6 +58,10 @@ class ExpensesController < ApplicationController
   # PATCH/PUT /expenses/1
   # PATCH/PUT /expenses/1.json
   def update
+    if !@current_user
+      redirect_to login_path
+    end
+
     respond_to do |format|
       if @expense.update(expense_params)
         format.html { redirect_to @expense, notice: 'Expense was successfully updated.' }
@@ -54,6 +76,10 @@ class ExpensesController < ApplicationController
   # DELETE /expenses/1
   # DELETE /expenses/1.json
   def destroy
+    if !@current_user
+      redirect_to login_path
+    end
+
     @expense.destroy
     respond_to do |format|
       format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
