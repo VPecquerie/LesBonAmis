81bf768e7c7f2ebffd3fe807897da8bf45c84a41
Sebastien Dugene <sebastien.dugene@gmail.com>
2017-08-29 09:46:54 +0200

    add user by scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :expenses
+  resources :users
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
