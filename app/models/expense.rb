f2cac253bc1bdea945e49fc9f2be7d6bfb889886
Florian Guillet <florian.guillet7@gmail.com>
2017-08-31 10:16:11 +0200

    Expenses: Add method payer and beneficiaries

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,12 @@ class Expense < ApplicationRecord
   has_and_belongs_to_many :users
 
   monetize :amount_cents, :as => "amount"
+
+  def payer
+    self.user
+  end
+
+  def beneficiaries
+    self.users
+  end
 end
