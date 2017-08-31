06cb1e703f91b1613a501d4f1666c782390beab9
Vincent PECQUERIE <contact@vincent-p.fr>
2017-08-31 12:23:16 +0200

    WIP on Expense mailer

---
+++ app/mailers/expense_mailer.rb
@@ -0,0 +1,8 @@
+class ExpenseMailer < ApplicationMailer
+    def expense_alert(expense)
+        @expense = expense
+        for beneficiary in expense.beneficiaries
+            mail(to: beneficiary.email, subject: expense.title)
+        end
+
+end
