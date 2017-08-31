96fedc73294c91b11691d3ea008101f76e982767
Florian Guillet <florian.guillet7@gmail.com>
2017-08-31 10:53:57 +0200

    Home: Move global balance on home page with redirect if user is not connected

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -98,33 +98,6 @@ class ExpensesController < ApplicationController
     end
   end
 
-  # GET /expenses/global
-  def global
-    if !@current_user
-      redirect_to login_path
-      return
-    end
-
-    @user = User.find(@current_user.id)
-    @expenses = @user.expenses
-
-    @user_spent = Money.new(0)
-    @users_debtor = Money.new(0)
-
-    @expenses.each do |expense|
-      @number_of_users = expense.users.count
-      @user_expense = expense.amount_money / @number_of_users
-
-      if expense.user_id == @user.id
-        @user_spent += @user_expense * (@number_of_users - 1)
-      else
-        @users_debtor += @user_expense
-      end
-    end
-
-    @global_user = @user_spent - @users_debtor
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_expense
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,28 @@
 class HomeController < ApplicationController
 
   def index
+    if !@current_user
+      redirect_to login_path
+      return
+    end
 
+    @user = User.find(@current_user.id)
+    @expenses = @user.expenses
+
+    @user_spent = Money.new(0)
+    @users_debtor = Money.new(0)
+
+    @expenses.each do |expense|
+      @number_of_users = expense.users.count
+      @user_expense = expense.amount / @number_of_users
+
+      if expense.user_id == @user.id
+        @user_spent += @user_expense * (@number_of_users - 1)
+      else
+        @users_debtor += @user_expense
+      end
+    end
+
+    @global_user = @user_spent - @users_debtor
   end
 end
